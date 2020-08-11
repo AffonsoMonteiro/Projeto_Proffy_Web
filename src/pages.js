f2efa288ab7469c04e116244cafa3e5e42c2b925
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2020-08-11 15:33:37 -0300

    config page-give-classes

--- src/pages.js
+++ src/pages.js
@@ -10,9 +10,7 @@ async function pageStudy(req, res) {
     const filters = req.query
 
     if(!filters.subject || !filters.weekday || !filters.time) {
-
         return res.render("study.html", { filters, subjects, weekdays })
-
     }
 
     // converter horas em minutos
@@ -38,6 +36,10 @@ async function pageStudy(req, res) {
         const db = await Database
         const proffys = await db.all(query)
 
+        proffys.map((proffy) => {
+            proffy.subject = getSubject(proffy.subject)
+        } )
+
         return res.render('study.html', { proffys, subjects, filters, weekdays })
 
     } catch (error) {
@@ -47,27 +49,51 @@ async function pageStudy(req, res) {
 }
 
 function pageGiveClasses(req, res) {
-    const data = req.query
-
-    // se tiver dados
-    const isNotEmpty = Object.keys(data).length > 0
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject)
-
-         //adicionar dados a lista de proffys
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-   
-
     // se nao, mostrar a pagina
     return res.render("give-classes.html", {subjects, weekdays})
 }
 
+async function saveClasses(req, res) {
+    const createProffy = require('./database/createProffy')
+    
+    const proffyValue = { 
+        name: req.body.name,
+        avatar: req.body.avatar,
+        whatsapp: req.body.whatsapp,
+        bio:req.body.bio
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost
+    }
+
+    const classScheduleValues = req.body.weekday.map((weekday, index) => {
+        return {
+            weekday,
+            time_from: convertHoursToMinutes(req.body.time_from[index]),
+            time_to: convertHoursToMinutes(req.body.time_to[index]) 
+        }
+
+    })
+
+    try {
+        const db = await Database
+        await createProffy(db, { proffyValue, classValue, classScheduleValues })
+        
+        let queryString ="?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+        return res.redirect("/study" + queryString)
+    } catch (error) {
+        console.log(error)
+    }
+
+} 
+
 module.exports = {
     pageLanding,
     pageStudy,
-    pageGiveClasses
+    pageGiveClasses,
+    saveClasses
 }
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const server = express()
 
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages')
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
 //configurar nunjucks (template engine)
 const nunjucks = require('nunjucks')
@@ -13,12 +13,15 @@ nunjucks.configure('src/views', {
 })
 
 //Inicio e configuração do servidor
-//configurar arquivos estáticos (css, scripts, imagens)
-
-server.use(express.static("public"))
+server
+// receber os dados fo req.body
+.use(express.urlencoded({extended: true }))
+//configurar arquivos estáticos (css, scripts, imagens
+.use(express.static("public"))
 //rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 // Start do servidor
-.listen(5500)
+.listen(5500)       
